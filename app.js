c013105823b9d491a05ac508f558c306bbfcb7a9
Fabian Garcia <fygarciaj@gmail.com>
2014-07-13 19:54:09 -0500

    probando la nueva configuracion

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-var express = require('express')
- , mongoose = require('mongoose')
- , stylus = require('stylus')
- , nib = require('nib')
- , controllers = require('./controllers')
- , config = require('./config')
- , models = require('./models')
- , db = mongoose.createConnection( config.mongodb.url );
+var express = require('express'),
+    mongoose = require('mongoose'),
+    stylus = require('stylus'),
+    nib = require('nib'),
+    controllers = require('./controllers'),
+    config = require('./config'),
+    models = require('./models'),
+    db = mongoose.createConnection(config.mongodb.url);
 
 /*
- var row = new models.users;
-    row.name = "Fabian Garcia",
-    row.email = "fygarciaj@gmail.com";
-    row.password = "6b59383bf5e823b168de73ddb0f937a2"; // polin
-    row.registered = new Date();
-    row.range = 1;
-    row.active = true;
-    row.save(function(err){
-      if(err){
+var row = new models.users;
+row.name = "Fabian Garcia",
+row.email = "fygarciaj@gmail.com";
+row.password = "6b59383bf5e823b168de73ddb0f937a2"; // polin
+row.registered = new Date();
+row.range = 1;
+row.active = true;
+row.save(function(err) {
+    if (err) {
         console.log(err);
-      }
-      else{
-       console.log(row.name + ' addeded');
-      }
-    });
+    } else {
+        console.log(row.name + ' addeded');
+    }
+});
 
 */
-function IsAuthenticated(req,res,next){
-  var path = ( req.path == null ) ? '/' : req.path;
-  if(req.session.user_id){
-    next();
-  }else{
-    res.redirect('/login?redirect='+path);
-  }
+function IsAuthenticated(req, res, next) {
+    var path = (req.path == null) ? '/' : req.path;
+    if (req.session.user_id) {
+        next();
+    } else {
+        res.redirect('/login?redirect=' + path);
+    }
 }
 
 function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
 }
 
 var app = express();
@@ -47,31 +46,32 @@ app.set('view engine', 'jade');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.cookieParser());
-app.use(express.session({ secret: '023197422617bce43335cbd3c675aeed' }));
+app.use(express.session({
+    secret: '023197422617bce43335cbd3c675aeed'
+}));
 app.use(express.logger('dev'));
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-));
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
 app.use(express.static(__dirname + '/public'));
 
 controllers.plataform.setup(models);
 
 // routes
-app.get("/",IsAuthenticated,controllers.plataform.homeGet);
-app.get("/login",controllers.plataform.loginGet);
-app.post('/login',controllers.plataform.loginPost);
-app.get("/logout",controllers.plataform.logoutGet);
-app.get("/sms/inbox",IsAuthenticated,controllers.plataform.smsInboxGet);
+app.get("/", IsAuthenticated, controllers.plataform.homeGet);
+app.get("/login", controllers.plataform.loginGet);
+app.post('/login', controllers.plataform.loginPost);
+app.get("/logout", controllers.plataform.logoutGet);
+app.get("/sms/inbox", IsAuthenticated, controllers.plataform.smsInboxGet);
 
 var server = require('http').createServer(app)
-var WebSocket = require("socket.io").listen( server );
+var WebSocket = require("socket.io").listen(server);
 
 controllers.api.setup(models, WebSocket);
 
 WebSocket.sockets.on("connection", controllers.api.webSocketStart);
 
-app.post("/api",controllers.api.receiber);
+app.post("/api", controllers.api.receiber);
 
 server.listen(9999);
--- controllers/api.js
+++ controllers/api.js
@@ -1,49 +1,51 @@
-var models
-, websocket
-, string = require('string')
-, moment = require('moment');
+var models, websocket, string = require('string'),
+    moment = require('moment');
 
 moment.lang("es");
-exports.setup = function( _models, _websocket ){
-	models = _models;
-	websocket = _websocket;
+exports.setup = function(_models, _websocket) {
+    models = _models;
+    websocket = _websocket;
 };
 
-function webSocketSendData (data){
-	data.text = string(decodeURIComponent(data.text.replace(/\+/g, '%20'))).stripTags().s;
-	data.registered = moment(new Date()).fromNow();
+function webSocketSendData(data) {
+    console.log(data);
+    data.text = string(decodeURIComponent(data.text.replace(/\+/g, '%20'))).stripTags().s;
 
-	var row = new models.sms;
-	row.number = data.number;
-	row.text = data.text;
-	row.registered = new Date();
-	row.read = false;
-	row.save(function(err){
-		if(err){
-			console.log(err);
-		}
-		else{
-			websocket.sockets.emit("ws_getData",data);
-		}
-	});
+    data.registered = moment(new Date()).fromNow();
+
+    var row = new models.sms;
+    row.number = data.number;
+    row.text = data.text;
+    row.registered = new Date();
+    row.read = false;
+    row.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            websocket.sockets.emit("ws_getData", data);
+        }
+    });
 }
 
 
-exports.webSocketStart = function (data){
-	function out(){
-		data.on("new_message", webSocketSendData);
-	}
-  return out();
+exports.webSocketStart = function(data) {
+    function out() {
+        data.on("new_message", webSocketSendData);
+    }
+    return out();
 }
 
 /**
  * Load login template form
  */
-exports.receiber = function(req, res){
-	function out(  ){
-		req.body.type="sms";
-		webSocketSendData(req.body);
-		res.send('true');
-	}
-	return out( );
+exports.receiber = function(req, res) {
+    function out() {
+        req.body.type = "sms";
+        // req.body.text = "Un mensaje";
+        // req.body.number = "1645432153";
+        console.log(req.body);
+        webSocketSendData(req.body);
+        res.send('true');
+    }
+    return out();
 }
